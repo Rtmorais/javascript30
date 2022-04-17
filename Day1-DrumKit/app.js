c67474d9fd3f4cbb97ae143f53a05fc92fe16b8f
Rtmorais <rt.morais@hotmail.com>
2022-04-17 04:00:27 -0300

    Done! Probably make some changes on styles later

--- Day1-DrumKit/app.js
+++ Day1-DrumKit/app.js
@@ -1,6 +1,15 @@
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  key.classList.add("playing");
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
+}
+
+window.addEventListener("keydown", playSound);
+
+window.addEventListener("keyup", function (e) {
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  key.classList.remove("playing");
 });
